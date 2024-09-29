a1b979491064a1e68c4437cebe92246a97137b46
flight <mwxj@mail.ru>
2024-09-29 19:50:44 +0300

    TR-1 initialization...

---
+++ back2reality/src/main/java/com/back2reality/Back2RealityApplication.java
@@ -0,0 +1,18 @@
+package com.back2reality;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Back2RealityApplication {
+
+  private static final Logger logger = LoggerFactory.getLogger(Back2RealityApplication.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(Back2RealityApplication.class, args);
+    logger.info("time to get back to reality...");
+  }
+
+}
---
+++ back2reality/src/test/java/com/back2reality/Back2RealityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back2reality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Back2RealityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
